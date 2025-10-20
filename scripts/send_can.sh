0b0f8fbf152d3b3942778afde27c3d0828445d87
vboxuser <vboxuser@ubuntugeneral.myguest.virtualbox.org>
2025-10-20 08:56:38 -0700

    changed send_can bash script to send temp data between 30 to 110

--- scripts/send_can.sh
+++ scripts/send_can.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 set -e
+# set can simulated data rate at 5HZ                                                                                                     
 RATE_HZ="${1:-5}"
 DT=$((1000 / RATE_HZ))
 
 echo "Simulating CAN traffic on vcan0 at ${RATE_HZ}Hz"
 
+#incrementing temp sensor data between 30.00°C and 40.00°C
 i=0
 while true; do
-  temp=$((3000 + (i % 1000)))   # 30.00°C–40.00°C
+  temp=$((30 + (i % 81)))   # 30.00°C–40.00°C
   hi=$(printf "%02X" $(( (temp >> 8) & 0xFF )))
   lo=$(printf "%02X" $(( temp & 0xFF )))
   cansend vcan0 100#${hi}${lo}
   cansend vcan0 101#01            # level OK
   cansend vcan0 102#01            # pump running
   cansend vcan0 200#41            # setpoint 65°C
-  echo "Sent frame batch $i (temp=$((temp/100)).$((temp%100))°C)"
+  echo "Sent frame batch $i (temp=${temp}°C)"
   i=$((i+1))
   sleep $(awk "BEGIN{print $DT/1000}")
 done
